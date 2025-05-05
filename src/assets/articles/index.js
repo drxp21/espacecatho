0696126721de984a3c3e4cc0c00cebead52cd13e
drxp21 <sdave6226@gmail.com>
2025-05-05 21:08:54 +0000

    refactor(assets): update image paths in articles index.js
    The image paths in the articles index.js file were updated to remove the '/src' prefix, ensuring consistency and correct asset referencing across the application. This change does not affect the behavior of the application but improves maintainability and avoids potential issues with asset loading.

--- src/assets/articles/index.js
+++ src/assets/articles/index.js
@@ -4,7 +4,7 @@ export const resources = [
       id: 'parcours',
       title: 'Le parcours du pèlerin',
       description: `C’est à la suite du Christ qui est « la Voie, la Vérité et la Vie » (Jn 14,6) et de la Vierge Marie « Porte du Ciel » que nous cheminons vers le Père. Le but de notre pèlerinage est de laisser le monde du péché et de la mort derrière nous pour entrer en communion avec Dieu, chaque année davantage, jusqu’à la rencontre finale face à face, au terme de notre vie sur cette terre…`,
-      image: '/src/assets/marcheurs.jpg',
+      image: '/assets/marcheurs.jpg',
       content:'HI tops'
 
     },
@@ -12,7 +12,7 @@ export const resources = [
       id: 'meditation',
       title: 'Méditation sur le thème',
       description: `Prenez un moment pour ,méditer sur le thème de cette année : « Marie, Mère de l’espérance, marche avec nous ». Cette méditation vous invite à approfondir votre foi, à réfléchir sur votre cheminement spirituel et à vous rapprocher de Dieu à travers la prière et la contemplation.`,
-      image: '/src/assets/preche.jpg',
+      image: '/assets/preche.jpg',
       content:'HI tops'
 
     },
@@ -20,7 +20,7 @@ export const resources = [
         id: 'neuvaine',
         title: 'Neuvaine préparatoire',
         description: `Préparez-vous spirituellement au pèlerinage en suivant la neuvaine dédiée à Marie. Chaque jour, une prière et une intention particulière vous guideront pour renforcer votre foi, demander des grâces et vous unir à la communauté des pèlerins dans la prière.`,
-        image: '/src/assets/lecture.jpg',
+        image: '/assets/lecture.jpg',
       content:'HI tops'
 
     }
